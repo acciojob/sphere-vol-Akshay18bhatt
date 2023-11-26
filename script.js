3e1260fc510cb6a98ee0e993753e9cefcda27001
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-26 19:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	let form= document.getElementById("MyForm");
+	let r=form.radius.value;
+	// console.log(r);
+	form.volume.value= ((4/3)*Math.PI* (r*r*r)).toFixed(4);
+	 event.preventDefault();
   
 } 
 
